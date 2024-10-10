32594822807348e7b60d2015ff3b42f86d8df15e
vinossauro <viniaraujoaraujo.va@gmail.com>
2024-10-10 17:15:02 -0300

    removing deprecated stuff

--- backend/server.js
+++ backend/server.js
@@ -18,8 +18,6 @@ app.use(express.json());
 
 // Connect to MongoDB
 mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
 }).then(() => {
   console.log('Connected to MongoDB');
 }).catch((error) => {
